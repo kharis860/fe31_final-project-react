5754f44f95372a70da513a8dfbd58f0d11bbfa32
fellianik <tanakanaomi04@gmail.com>
2022-11-18 18:07:43 +0700

    menambahkan login dan handlenya

--- src/App.js
+++ src/App.js
@@ -6,21 +6,23 @@ import { Routes, Route, Link } from "react-router-dom";
 import DataPasien from "./components/DataPasien";
 import Konsultasi from "./components/Konsultasi";
 import InnerDashboard from "./components/InnerDashboard";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    // <div>
-    //   <Sidebar />
-    //   <Navbar />
-    // </div>
-    <Routes>
-      <Route path="/" element={<Dashboard />}>
-        <Route path="/" element={<InnerDashboard />} />
-        <Route path="/data" element={<DataPasien />} />
-        <Route path="/konsultasi" element={<Konsultasi />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        // <div>
+        //   <Sidebar />
+        //   <Navbar />
+        // </div>
+        <Routes>
+            <Route path="/" element={<Dashboard />}>
+                <Route path="/" element={<InnerDashboard />} />
+                <Route path="/data" element={<DataPasien />} />
+                <Route path="/konsultasi" element={<Konsultasi />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+        </Routes>
+    );
 }
 
 export default App;
