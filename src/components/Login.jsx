89a84ffed1027b7abbccd31804e3c96849196862
fellianik <tanakanaomi04@gmail.com>
2022-11-18 20:31:47 +0700

    login handle

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 import bgLogin from "../Img/bgLogin.png";
 import logo from "../Img/Medtech_sm.png";
@@ -11,13 +11,15 @@ function Login() {
     const [isLogin, setIsLogin] = useState(false);
     const [dataDokter, setDataDokter] = useState();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-
+    useEffect(() => {
         axios.get("https://6350e03cdfe45bbd55b074ed.mockapi.io/medTechAPI/users").then((response) => {
             setDataDokter(response.data);
             // console.log(dataDokter);
         });
+    }, []);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
         for (let i = 0; i < dataDokter.length; i++) {
             if (username === dataDokter[i].username && password === dataDokter[i].password) {
@@ -30,46 +32,45 @@ function Login() {
         } else {
             alert("Masukkan username atau password yang benar");
         }
-
-        // setUsername("");
-        // setPassword("");
     };
 
     return (
-        <div id="login" onSubmit={handleSubmit}>
-            <div className="container-fluid">
-                <section className="login d-flex">
-                    <div className="login-left w-50 h-50">
-                        <div className="row justify-content-center align-items-center">
-                            <div className="img-logo text-center">
-                                <img src={logo} alt="Logo Medtech" className="img-logo img-thumbnail" style={{ backgroundColor: "#405262", borderColor: "#405262" }} />
+        <div id="login-bg">
+            <div id="login" onSubmit={handleSubmit}>
+                <div className="container-fluid">
+                    <section className="login d-flex">
+                        <div className="login-left w-50 h-50">
+                            <div className="row justify-content-center align-items-center">
+                                <div className="img-logo text-center">
+                                    <img src={logo} alt="Logo Medtech" className="img-logo img-thumbnail" style={{ backgroundColor: "#405262", borderColor: "#405262" }} />
+                                </div>
+                            </div>
+                            <div className="col-6">
+                                <div className="header">
+                                    <h1>Login</h1>
+                                </div>
+                                <div className="login-form">
+                                    <form id="userForm">
+                                        <label htmlFor="username" className="form-label"></label>
+                                        <input type="text" className="form-control" id="username" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)} />
+
+                                        <label htmlFor="password" className="form-label"></label>
+                                        <input type="password" className="form-control" id="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
+
+                                        <div className="text-center">
+                                            <button type="submit" className="sign-in align-items-center">
+                                                Login
+                                            </button>
+                                        </div>
+                                    </form>
+                                </div>
                             </div>
                         </div>
-                        <div className="col-6">
-                            <div className="header">
-                                <h1>Login</h1>
-                            </div>
-                            <div className="login-form">
-                                <form id="userForm">
-                                    <label htmlFor="username" className="form-label"></label>
-                                    <input type="text" className="form-control" id="username" placeholder="Username" required value={username} onChange={(e) => setUsername(e.target.value)} />
-
-                                    <label htmlFor="password" className="form-label"></label>
-                                    <input type="password" className="form-control" id="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-
-                                    <div className="text-center">
-                                        <button type="submit" className="sign-in align-items-center">
-                                            Login
-                                        </button>
-                                    </div>
-                                </form>
-                            </div>
+                        <div className="login-right w-40 h-50">
+                            <img src={bgLogin} alt="Background Login" className="bg-login" />
                         </div>
-                    </div>
-                    <div className="login-right w-40 h-50">
-                        <img src={bgLogin} alt="Background Login" className="bg-login" />
-                    </div>
-                </section>
+                    </section>
+                </div>
             </div>
         </div>
     );
